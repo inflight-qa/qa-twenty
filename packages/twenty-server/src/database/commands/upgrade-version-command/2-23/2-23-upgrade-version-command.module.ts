89acd354af26f19be1a8e2435d211ed60280f5e7
prastoin <paul@twenty.com>
2026-07-17 12:07:51 +0200

    upgrade people data labs

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-23/2-23-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-23/2-23-upgrade-version-command.module.ts
@@ -3,17 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
 import { ReconcileSystemRelationFieldUniversalIdentifierCommand } from 'src/database/commands/upgrade-version-command/2-23/2-23-workspace-command-1784276808000-reconcile-system-relation-field-universal-identifier.command';
+import { UpgradePeopleDataLabsApplicationCommand } from 'src/database/commands/upgrade-version-command/2-23/2-23-workspace-command-1784281825000-upgrade-people-data-labs-application.command';
+import { ApplicationUpgradeModule } from 'src/engine/core-modules/application/application-upgrade/application-upgrade.module';
+import { ApplicationEntity } from 'src/engine/core-modules/application/application.entity';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
 import { WorkspaceMigrationRunnerModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-runner/workspace-migration-runner.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([FieldMetadataEntity]),
+    TypeOrmModule.forFeature([FieldMetadataEntity, ApplicationEntity]),
     WorkspaceMigrationRunnerModule,
     WorkspaceCacheModule,
     WorkspaceIteratorModule,
+    ApplicationUpgradeModule,
   ],
-  providers: [ReconcileSystemRelationFieldUniversalIdentifierCommand],
+  providers: [
+    ReconcileSystemRelationFieldUniversalIdentifierCommand,
+    UpgradePeopleDataLabsApplicationCommand,
+  ],
 })
 export class V2_23_UpgradeVersionCommandModule {}
